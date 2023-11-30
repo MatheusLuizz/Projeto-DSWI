e97bf247bf80c551033dc6c905e0ae3d2ba857ba
Matheus Luiz <matheus.luizf35@gmail.com>
2023-11-30 15:12:46 -0300

    adicionando css básico, script de localização e páginas iniciais

---
+++ script.js
@@ -0,0 +1,17 @@
+navigator.geolocation.getCurrentPosition(function (posicao) {
+    var url = "http://nominatim.openstreetmap.org/reverse?lat=" + posicao.coords.latitude + "&lon=" + posicao.coords.longitude + "&format=json&json_callback=preencherDados";
+
+    var script = document.createElement('script');
+    script.src = url;
+    document.body.appendChild(script);
+});
+
+function preencherDados(dados) {
+    // Verifica se há informações de estado nos dados
+    if (dados.address && dados.address.state) {
+        document.getElementById("location").innerText = `${dados.address.state}`;
+    } else {
+        // Se o estado não estiver disponível, define uma mensagem de erro
+        document.getElementById("location").innerText = "Informações de estado não encontradas";
+    }
+}
