7518460fe29048dc348e6138cfa23f559d015f87
Matheus Luiz <matheus.luizf35@gmail.com>
2023-12-07 19:26:05 -0300

    Adicionando novas telas e estilos

--- script.js
+++ script.js
@@ -15,3 +15,23 @@ function preencherDados(dados) {
         document.getElementById("location").innerText = "Informações de estado não encontradas";
     }
 }
+
+const imgs = document.getElementById("img")
+const img = document.querySelectorAll("#img img")
+
+let idx = 0;
+function carrossel() {
+    idx++
+
+    if (idx > img.length - 1) {
+        idx = 0
+    }
+
+    imgs.style.transform = `translateX(${-idx * 500}px)`
+}
+function toggleNav() {
+    var links = document.querySelector('.links');
+    links.style.display = (links.style.display === 'none' || links.style.display === '') ? 'flex' : 'none';
+}
+
+setInterval(carrossel, 1800)
